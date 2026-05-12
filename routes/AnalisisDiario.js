bb7a68dbfee7b0c3012437a1a442bdd2b4ae782e
yeniferquintero <yen.quimo@gmail.com>
2026-05-12 21:27:36 +0200

    historial analisis

--- routes/AnalisisDiario.js
+++ routes/AnalisisDiario.js
@@ -9,7 +9,7 @@ const { formatDate } = require('../utils/date');
 router.post('/', async (req, res) => {
   try {
     const usuarioId = req.usuario._id || req.usuario.id;
-    const registros = await RegistroEmocional.find({userId: usuarioId}).sort({ createdAt: -1 }).limit(7).lean();
+    const registros = await RegistroEmocional.find({ userId: usuarioId }).sort({ createdAt: -1 }).limit(7).lean();
 
     if (!registros.length) {
       return res.status(404).json({
@@ -26,29 +26,57 @@ router.post('/', async (req, res) => {
       { upsert: true, new: true }
     );
 
-    return res.json({ok: true,analisis});
+    return res.json({ ok: true, analisis });
 
   } catch (error) {
-    console.error('ERROR ANALISIS:',error);
+    console.error('ERROR ANALISIS:', error);
 
-    return res.status(500).json({ok: false,message:'error_generando_analisis'});
+    return res.status(500).json({ ok: false, message: 'error_generando_analisis' });
   }
 }
 );
-
-router.get('/:fecha', async (req, res) => {
+router.get('/historial', async (req, res) => {
   try {
-    const analisis = await Analisis.findOne({usuarioId: req.usuario._id || req.usuario.id,fechaClave: req.params.fecha});
-  console.log("REQ.USUARIO:", req.usuario);
-    if (!analisis) {
-      return res.status(404).json({ok: false,message:'analisis_no_encontrado'});
-    }
 
-    return res.json({ok: true,analisis});
+    const usuarioId =
+      req.usuario._id || req.usuario.id;
+
+    const analisis =
+      await Analisis.find({ usuarioId })
+        .sort({ actualizadoEn: -1 })
+        .limit(7)
+        .lean();
+
+    return res.json({
+      ok: true,
+      analisis
+    });
 
   } catch (error) {
-    console.error('ERROR GET ANALISIS:',error);
-    return res.status(500).json({ok: false});
+
+    console.error(
+      'ERROR HISTORIAL ANALISIS:',
+      error
+    );
+
+    return res.status(500).json({
+      ok: false
+    });
+  }
+});
+router.get('/:fecha', async (req, res) => {
+  try {
+    const analisis = await Analisis.findOne({ usuarioId: req.usuario._id || req.usuario.id, fechaClave: req.params.fecha });
+    console.log("REQ.USUARIO:", req.usuario);
+    if (!analisis) {
+      return res.status(404).json({ ok: false, message: 'analisis_no_encontrado' });
+    }
+
+    return res.json({ ok: true, analisis });
+
+  } catch (error) {
+    console.error('ERROR GET ANALISIS:', error);
+    return res.status(500).json({ ok: false });
   }
 }
 );
